9a0907ab8f71d0cdcf963192f3f052a662dd7edc
paul449 <paul_bilbatua@hotmail.com>
2024-04-12 14:52:15 -0500

    adding tables to schema.sql and creating seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS Employees;
+
+CREATE DATABASE Employees;
+
+/c:Employees.db;
+
+CREATE TABLE department(
+    id: SERIAL PRIMARY KEY,
+    name: VARCHAR(30) UNIQUE NOT NULL,
+);
+CREATE TABLE role(
+    id: SERIAL PRIMARY KEY,
+    title: VARCHAR(30) UNIQUE NOT NULL,
+    salary: DECIMAL NOT NULL,
+    department_id: INTEGER NOT NULL,
+);
+CREATE TABLE employee(
+    id: SERIAL PRIMARY KEY,
+    firstName: VARCHAR(30) UNIQUE NOT NULL,
+    lastName: VARCHAR(30) UNIQUE NOT NULL,
+    role_id: INTEGER NOT NULL,
+    manager_id: INTEGER,
+);
